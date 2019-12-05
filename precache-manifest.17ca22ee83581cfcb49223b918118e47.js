79e1e16cf9637645ac84a9d5644f1cefd295e713
Kristina Lindmark <krli008@users.noreply.github.com>
2019-12-05 13:10:40 +0100

    Updates

---
+++ precache-manifest.17ca22ee83581cfcb49223b918118e47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac706724433e5a9847b8074f1d074ab6",
+    "url": "/tema-oppgave/index.html"
+  },
+  {
+    "revision": "72f9afb772f0c8edf5fa",
+    "url": "/tema-oppgave/static/css/main.14a31a89.chunk.css"
+  },
+  {
+    "revision": "f73378291e28021adbe5",
+    "url": "/tema-oppgave/static/js/2.3968d37f.chunk.js"
+  },
+  {
+    "revision": "72f9afb772f0c8edf5fa",
+    "url": "/tema-oppgave/static/js/main.816a783b.chunk.js"
+  },
+  {
+    "revision": "caabd81ca37e673e0460",
+    "url": "/tema-oppgave/static/js/runtime-main.2f790c18.js"
+  }
+]);
